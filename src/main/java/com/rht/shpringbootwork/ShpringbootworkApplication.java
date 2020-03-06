2030de7304e50acd1dc068c9ffe03e5ac3116ef3
rhtwork <1763260945@qq.com>
2020-03-06 15:00:00 +0800

    first commit

---
+++ src/main/java/com/rht/shpringbootwork/ShpringbootworkApplication.java
@@ -0,0 +1,13 @@
+package com.rht.shpringbootwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShpringbootworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShpringbootworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rht/shpringbootwork/ShpringbootworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rht.shpringbootwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShpringbootworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
